a59075db047de26e46723d6aeed1fcd5a4146c47
vermaparul85 <verma.parul.85@gmail.com>
2024-08-31 17:08:11 +0530

    Create Calculator-Method-2.py

---
+++ Calculator/Calculator-Method-2.py
@@ -0,0 +1,40 @@
+from IPython.display import clear_output
+from art import calc_logo
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+# Using function as valiable 
+calc_dict = {'+': add, '-': subtract, '*': multiply, '/': divide} 
+
+def calculator():
+    print(calc_logo)
+    first_number = float(input('What is your first number? '))
+    
+    while True:
+        for symbol in calc_dict:
+            print(symbol)
+        operation = input('Pick an operation? ')
+        next_number = float(input('What is your next number? '))
+        
+        result = calc_dict[operation](first_number,next_number)
+        print(f'{first_number} {operation} {next_number} = {result}')
+        
+        continue_calculating = input(f'Type "y" to continue calculating with {result}, or type "n" to start a new calculation: ').lower()
+    
+        if continue_calculating == 'y':
+            first_number = result
+        else:
+            clear_output()
+            calculator()           #Recursion
+        
+calculator()
